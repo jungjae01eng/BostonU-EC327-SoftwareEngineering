6d09b49ee7a5125d2258b3e977d67f30568cdd37
Jungjae <100228417+jungjae01eng@users.noreply.github.com>
2022-10-08 17:28:40 -0400

    Add files via upload

---
+++ PA_1_3_Hamming_Distance.cpp
@@ -0,0 +1,26 @@
+// Jungjae Lee
+// Boston University College of Engineering
+// EC 327 Programming Assignment 1 Question 3
+
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+int main()
+{
+    // initializing number variable
+    int counter;
+
+    // declare array - It will initialize 6 bits
+    int num1[6], num2[6];
+
+
+    // receive numbers in between 0 to 100 from user
+    cout << "Enter two numbers between 0-100: ";
+    cin >> num1 >> num2;
+
+    if 
+    // convert decimal to ternary
+    
+}
