6753ae07ed18f7201278a840154677991817b0f9
Jungjae <100228417+jungjae01eng@users.noreply.github.com>
2022-10-07 21:36:34 -0400

    Add files via upload

--- PA_1_2_Converting_Units.cpp
+++ PA_1_2_Converting_Units.cpp
@@ -3,7 +3,7 @@
 // EC 327 Programming Assignment 1 Question 2
 
 #include <iostream>
-#include math.h
+#include "math.h"
 
 using namespace std;
 
@@ -24,35 +24,55 @@ int main()
         cout << "Kelvin to Fahrenheit (enter 5)" << endl;
         cout << "Conversion type: ";
         cin >> type;
-    } while (type <= 0 || type < 6);
+    } while (type <= 0 || type > 6);
 
     if (type == 0)
     {
         // Celsius to Fahrenheit
-        cout << "Enter the amount in Celsius: " cin >> celsius;
+        cout << "Enter the amount in Celsius: ";
+        cin >> celsius;
         fahrenheit = (celsius * 9 / 5) + 32;
-        cout << celsius << "Celsius is " << fahrenheit << "Fahrenheit. [Program Exits]"
+        cout << celsius << " Celsius is " << fahrenheit << " Fahrenheit. [Program Exits]" << endl;
     }
     else if (type == 1)
     {
         // Celsius to Kelvin
-        cout << "Enter the amount in Celsius: " cin >> celsius;
+        cout << "Enter the amount in Celsius: ";
+        cin >> celsius;
         kelvin = celsius + 273.15;
-        cout << celsius << "Celsius is " << kelvin << "Kelvin. [Program Exits]"
+        cout << celsius << " Celsius is " << kelvin << " Kelvin. [Program Exits]" << endl;
     }
     else if (type == 2)
     {
         // Fahrenheit to Celsius
-        cout << "Enter the amount in Celsius: " cin >> fahrenheit;
+        cout << "Enter the amount in Celsius: ";
+        cin >> fahrenheit;
         celsius = (fahrenheit - 32) * 5 / 9;
-        cout << fahrenheit << "Fahrenheit is " << celsius << "Celsius. [Program Exits]"
+        cout << fahrenheit << " Fahrenheit is " << celsius << " Celsius. [Program Exits]" << endl;
     }
     else if (type == 3)
     {
         // Fahrenheit to Kelvin
-        cout << "Enter the amount in Celsius: " cin >> fahrenheit;
+        cout << "Enter the amount in Celsius: ";
+        cin >> fahrenheit;
         kelvin = (fahrenheit - 32) * 5 / 9 + 273.15;
-        cout << fahrenheit << "Fahrenheit is " << kelvin << "Kelvin. [Program Exits]"
+        cout << fahrenheit << " Fahrenheit is " << kelvin << " Kelvin. [Program Exits]" << endl;
+    }
+    else if (type == 4)
+    {
+        // Kelvin to Celsius
+        cout << "Enter the amount in Kelvin: ";
+        cin >> kelvin;
+        celsius = kelvin - 273.15;
+        cout << kelvin << " Kelvin is " << celsius << " Celsius. [Program Exits]" << endl;
+    }
+    else if (type == 5)
+    {
+        // Kelvin to Fahrenheit
+        cout << "Enter the amount in Kelvin: ";
+        cin >> kelvin;
+        fahrenheit = (kelvin - 273.15) * 9 / 5 +32;
+        cout << kelvin << " Kelvin is " << fahrenheit << " Fahrenheit. [Program Exits]" << endl;
     }
 
     return 0;
