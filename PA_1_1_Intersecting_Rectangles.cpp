a619cfda9e26d8052841aba4d90f8836aa2b9891
Jungjae <100228417+jungjae01eng@users.noreply.github.com>
2022-10-10 11:50:38 -0400

    Add files via upload

--- PA_1_1_Intersecting_Rectangles.cpp
+++ PA_1_1_Intersecting_Rectangles.cpp
@@ -8,10 +8,10 @@
 using namespace std;
 
 void print_Intersect() {
-    cout << "THE RECTANGLES INTERSECT [Program Exits" << endl;
+    cout << "THE RECTANGLES INTERSECT [Program Exits]" << endl;
 }
 void print_Do_Not_Intersect() {
-    cout << "THE RECTANGLES DO NOT INTERSECT [Program Exits" << endl;
+    cout << "THE RECTANGLES DO NOT INTERSECT [Program Exits]" << endl;
 }
 
 int main()
@@ -34,7 +34,7 @@ int main()
     cin >> first_height;
 
     // receive informations on SECOND rectangle from user
-    cout << "Enter the information for the first rectangle" << endl;
+    cout << "Enter the information for the second rectangle" << endl;
     cout << "x-coordinate: ";
     cin >> second_x;
     cout << "y-coordinate: ";
@@ -45,55 +45,66 @@ int main()
     cin >> second_height;
 
 
-    // calculate FIRST rectangle location
+    // calculate rectangles location
     first_x_lenghh = first_x + first_length;
     first_y_height = first_y + first_height;
 
 
-    if ((second_y - first_y) <= first_length) {
-        if (second_x >= first_x) {  // if true, second_x is on the RIGHT side of first_x
-            if ((second_y - first_y) >= 0) {    // if true, second_y is ABOVE the first_y
+    if (((second_y - first_y) <= first_height) || ((first_y - second_y) <= second_height)) { // rectangle 1 and 2 are on the same range in y-aixes
+        if (second_x >= first_x) {  // second_x is on the RIGHT side of first_x
+            if ((second_y - first_y) >= 0) {    // second_y is ABOVE the first_y
                 if ((second_y - first_y) > first_height) {
+                    // cout << "1" << endl;
                     print_Do_Not_Intersect();
                     return 0;
                 }
                 else if ((second_y - first_y) <= first_height) {
+                    // cout << "2" << endl;
                     print_Intersect();
                     return 0;
                 }
             }
-            else if ((second_y - first_y) < 0) {    // if true, second_y is BELOW the first_y
-                if ((second_y - first_y) > second_height) {
-                    print_Do_Not_Intersect();
-                    return 0;
-                }
-                else if ((second_y - first_y) <= second_height) {
-                    print_Intersect();
-                    return 0;
-                }
-            }
-        }
-        else if (second_x < first_x) {  // if true, second_x is on the LEFT side of first_x
-            if ((first_y - second_y) >= 0) {    // if true, first_y is ABOVE the second_y
-                if ((first_y - second_y) > first_height) {
-                    print_Do_Not_Intersect();
-                    return 0;
-                }
-                else if ((first_y - second_y) <= first_height) {
-                    print_Intersect();
-                    return 0;
-                }
-            }
-            else if ((first_y - second_y) < 0) {    // if true, first_y is BELOW the second_y
+            else if ((second_y - first_y) < 0) {    // second_y is BELOW the first_y
                 if ((first_y - second_y) > second_height) {
+                    // cout << "3" << endl;
                     print_Do_Not_Intersect();
                     return 0;
                 }
                 else if ((first_y - second_y) <= second_height) {
+                    // cout << "4" << endl;
                     print_Intersect();
                     return 0;
                 }
             }
         }
+        else if (second_x < first_x) {  // second_x is on the LEFT side of first_x
+            if ((first_y - second_y) >= 0) {    // first_y is ABOVE the second_y
+                if ((first_y - second_y) > second_height) {
+                    // cout << "5" << endl;
+                    print_Do_Not_Intersect();
+                    return 0;
+                }
+                else if ((first_y - second_y) <= second_height) {
+                    // cout << "6" << endl;
+                    print_Intersect();
+                    return 0;
+                }
+            }
+            else if ((second_y - first_y) > 0) {    // first_y is BELOW the second_y
+                if ((second_y - first_y) > first_height) {
+                    // cout << "7" << endl;
+                    print_Do_Not_Intersect();
+                    return 0;
+                }
+                else if ((second_y - first_y) <= first_height) {
+                    // cout << "8" << endl;
+                    print_Intersect();
+                    return 0;
+                }
+            }
+        }
+    }
+    else {
+        print_Do_Not_Intersect();
     }
 }
