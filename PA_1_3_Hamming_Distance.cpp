448b625e467a89c80d74f2c768d680c8dbf68219
Jungjae <100228417+jungjae01eng@users.noreply.github.com>
2022-10-09 23:37:15 -0400

    Add files via upload

--- PA_1_3_Hamming_Distance.cpp
+++ PA_1_3_Hamming_Distance.cpp
@@ -7,20 +7,33 @@
 
 using namespace std;
 
+
 int main()
 {
     // initializing number variable
     int counter;
+    int num1, num2; // User Input
+    //int counter;
+    int result1;
+    int remainder[6];
 
     // declare array - It will initialize 6 bits
-    int num1[6], num2[6];
+    // int num1[6], num2[6];
 
 
-    // receive numbers in between 0 to 100 from user
-    cout << "Enter two numbers between 0-100: ";
-    cin >> num1 >> num2;
+    do {
+        // receive numbers in between 0 to 100 from user
+        cout << "Enter two numbers between 0-100: ";
+        cin >> num1;
+        cin >> num2;
+    } while (num1 < 0 || num1 > 100);
 
-    if 
-    // convert decimal to ternary
-    
+
+    for (int counter = 0; counter < 6; counter++) {
+        for (int counter_exp = 0; counter_exp < 3; counter_exp++){
+            remainder[counter] = num1 % (int) round (pow(3, counter));
+        }
+    }
+    cout << remainder[6] << endl;
+    cout << "9";
 }
