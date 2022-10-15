9b9f084191b9a6cbe0fb74d01552fc384ff94a4c
Jungjae <100228417+jungjae01eng@users.noreply.github.com>
2022-10-14 21:06:27 -0400

    Add files via upload

---
+++ Lab_2_Math_Trick.cpp
@@ -0,0 +1,18 @@
+// Jungjae Lee
+// Boston University College of Engineering
+// EC 327 Lab 2 Question 4
+
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+  int num = 0;
+
+  cin >> num;
+  cout << (num*8+2)/3-num << endl;
+
+
+  return 0;
+}
