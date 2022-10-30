400119c6f2b34b328b2ecb3456eb3b35f1fd02b2
Jungjae <100228417+jungjae01eng@users.noreply.github.com>
2022-10-30 12:42:33 -0400

    Add files via upload

---
+++ PA_2_3_Reverse_multiply.cpp
@@ -0,0 +1,23 @@
+// Jungjae Lee
+// Boston University College of Engineering
+// EC 327 Programming Assignment 2 Question 3
+// October 30, 2022
+
+
+#include <iostream>  
+#include <iomanip>  // for setprecision
+
+using namespace std;
+
+int *ReverseMultiply (int *list, int size);
+int *ReverseAdd (int *list, int size);
+
+
+int main(){
+    int numEntry;
+
+    cout << "Enter the number of entries: ";
+    cin >> numEntry;
+
+
+}
