a7e00fb62629732ca605479942fe9277e7045e8b
Jungjae <100228417+jungjae01eng@users.noreply.github.com>
2022-10-30 12:56:25 -0400

    Add files via upload

---
+++ PA_2_1_Functions.cpp
@@ -0,0 +1,27 @@
+// Jungjae Lee
+// Boston University College of Engineering
+// EC 327 Programming Assignment 2 Question 1
+// October 30, 2022
+
+
+#include <iostream>  
+#include <iomanip>  // for setprecision
+
+using namespace std;
+
+
+void initialize();
+bool checkCode(char);
+void writeDataToFile(const char*);
+void readDatafromFile(const char*);
+int factorial(int);
+int fibonacci(int);
+double findSqrtValue(double);
+double naturalLog(double);
+double areaCircle(double);
+double areaSquare(double);
+int findNextOddValue(int);
+int findNextEvenValue(int);
+double findNyanCatValue(double);
+double doMath(double, char);
+double lucky(double);
---
+++ PA_2_1_Main.cpp
@@ -0,0 +1,15 @@
+// Jungjae Lee
+// Boston University College of Engineering
+// EC 327 Programming Assignment 2 Question 1
+// October 30, 2022
+
+
+#include <iostream>  
+#include <iomanip>  // for setprecision
+
+using namespace std;
+
+
+int main(){
+    
+}
